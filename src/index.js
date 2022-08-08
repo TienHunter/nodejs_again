6672d09551d1312d6cf8c70baecc82c7cab076b3
TienHunter <tienkbtnhp@gmail.com>
2022-08-08 19:53:15 +0700

    fix commit

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 // HTTP protocol
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 // Template engine
 app.engine('.hbs', engine({ extname: '.hbs' }) /** Configuration */);
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views') /** graft path */);
 // console.log(__dirname);
+
+// Set static folder
+// app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req, res) => {
    res.render('home')
 })
